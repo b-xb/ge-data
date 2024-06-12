f49f4046e34fe5d6a4ab9dc28ca62dceaa2de69d
b-xb <7443583+b-xb@users.noreply.github.com>
2024-06-12 07:40:34 +0100

    Enable filtering to show only constituencies that voted Leave or Remain at the 2016 EU Referendum

--- src/components/ChartWrapper.vue
+++ src/components/ChartWrapper.vue
@@ -3,6 +3,7 @@
     <h1 style="font-size:5vmin">UK General Election Data Visualiser</h1>
     <div style="position:relative; height:100vmin; width:70vmin; margin:auto;">
       <line-chart :chart-data="datacollection" :options="options"/>
+      <label>Political Party: </label>
       <select v-model="party">
         <option value="labour">Labour</option>
         <option value="tories">Tories</option>
@@ -11,6 +12,13 @@
         <option value="ukip">UKIP</option>
         <option value="brexit">UKIP/Brexit Party</option>
         <option value="toriesxbrexit">Tories + UKIP/BP</option>
+      </select>
+      <br>
+      <label>EU Referendum Vote: </label>
+      <select v-model="euvote">
+        <option value="all">-disabled-</option>
+        <option value="remain">>50% Remain</option>
+        <option value="leave">>50% Leave</option>
       </select>
     </div>
   </div>
@@ -71,6 +79,12 @@ export default {
         brexit: [["UKIP 2015"],["UKIP 2017"],["UKIP","Brex"]],
         toriesxbrexit: [["Con 2015","UKIP 2015"],["Con 2017","UKIP 2017"],["Con","UKIP","Brex"]],
       },
+      euvote: "all",
+      euvotelogic: {
+        all: () => true,
+        remain: (constituency) => constituency["Leave vote (%) 2016"]<50,
+        leave: (constituency) => constituency["Leave vote (%) 2016"]>50,
+      },
       bgColor: {
         labour: "rgb(255,0,0)",
         tories: "rgb(0,0,255)",
@@ -106,21 +120,27 @@ export default {
     fillData () {
       this.datacollection = {
         labels: ["2015","2017","2019"],
-        datasets: Array.from(this.dataSource,(constituency) => ({
-          label: constituency["Constituency"],
-          backgroundColor: this.bgColor[this.party],
-          borderColor: this.lineColor[this.party],
-          fill: false,
-          data: this.cols[this.party].map((cols) =>
-            cols.reduce((colSum, col) => colSum + parseFloat(constituency[col]),0)
-          )
-        }))
+        datasets: Array.from(
+          this.dataSource.filter(this.euvotelogic[this.euvote]),
+          (constituency) => ({
+            label: constituency["Constituency"],
+            backgroundColor: this.bgColor[this.party],
+            borderColor: this.lineColor[this.party],
+            fill: false,
+            data: this.cols[this.party].map((cols) =>
+              cols.reduce((colSum, col) => colSum + parseFloat(constituency[col]),0)
+            )
+          })
+        )
       }
     }
   },
 
   watch: {
     party() {
+      this.fillData();
+    },
+    euvote() {
       this.fillData();
     }
   }
