71e713135c1171a2abaf5e01d655f7dca9496389
b-xb <7443583+b-xb@users.noreply.github.com>
2024-06-12 06:41:11 +0100

    Use https instead of ssh for deployment

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:`git config user.name`/ge-data.git master:gh-pages
+git push -f https://github.com/`git config user.name`/ge-data.git main:gh-pages
 
 cd -
